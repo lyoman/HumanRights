54f00ac395fb5150ec5f9651311085286465bf79
hmupfumi <h.mupfumi@delta.co.zw>
2022-06-06 05:06:45 +0200

    done with chnges

--- src/app/addstock.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AddstockPipe } from './addstock.pipe';
-
-describe('AddstockPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AddstockPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
